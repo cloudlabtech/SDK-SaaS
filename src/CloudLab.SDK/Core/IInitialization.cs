b60c96d719644cc7c10cea1aa1609004ce8aa857
Eric Darruiz <33941948+edarruiz@users.noreply.github.com>
2023-04-11 15:05:42 -0300

    Create IInitialization.cs

---
+++ src/CloudLab.SDK/Core/IInitialization.cs
@@ -0,0 +1,17 @@
+﻿namespace CloudLab.SDK.Context;
+
+/// <summary>
+/// Represents the implementation contract for classes that supports initialization.
+/// </summary>
+public interface IInitialization {
+    /// <summary>
+    /// Gets or sets if the instance was already initialized.
+    /// </summary>
+    bool IsInitialized { get; }
+
+    /// <summary>
+    /// Initializes the object instance.
+    /// </summary>
+    /// <returns>Returns <c>true</c> when the object was successfully initialized; <c>false</c> otherwise.</returns>
+    bool Initialize();
+}
